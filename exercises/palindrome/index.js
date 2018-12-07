6e558697f83a354860bbbc443e3d52aa9428e113
cmlugoce <cmlugoce@syr.edu>
2018-12-07 13:57:40 -0800

    add palindrome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    let splitted = str.split("");
+    let r= splitted.reverse();
+    let jstr= r.join("");
+
+    return str === jstr;
+}
 
 module.exports = palindrome;
