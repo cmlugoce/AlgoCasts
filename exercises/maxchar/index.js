81b6726f8ceeac5656c7e4ea241a6277a9c5531d
cmlugoce <cmlugoce@syr.edu>
2018-12-07 16:14:51 -0800

    add solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+    for (let char of str) {
+     if(!chars[char]){
+       chars[char] = 1;
+    } else {
+      chars[char] ++;
+    }
+ }
+ //console.log(chars);
+ let max = 0;
+let maxChar = '';
+for (let char in chars){
+ if (chars[char] > max){
+   max = chars[char];
+   maxChar = char;
+ }
+}
+return maxChar;
+}
+
+const str = "hey there"
 
 module.exports = maxChar;
