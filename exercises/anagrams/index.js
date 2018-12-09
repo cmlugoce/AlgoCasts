86a9b4638595447ce4b45cae72d90f030532d755
cmlugoce <cmlugoce@syr.edu>
2018-12-09 07:18:10 -0800

    add solutions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+ const aCharMap = makeCharMap(stringA);
+ const bCharMap = makeCharMap(stringB);
+
+ //look at the number of keys and count them
+
+ if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+     return false;
+ }
+ //iterate over object
+ for (let char in aCharMap){
+    if (aCharMap[char] !== bCharMap[char]){
+        return false;
+    }
+ }
+ return true;
+}
+
+function makeCharMap(str) {
+    const charMap = {};
+    for(let char of str.replace(/[^\w]/g, '').toLowerCase()){
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
 
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,34 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+   return str.toLowerCase().split(' ')
+    .map((s)=> s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+}
 
 module.exports = capitalize;
+
+
+//.charAt() -> The String object's charAt() method returns a new string consisting of the single UTF-16 code unit located at the specified offset into the string.
+ /* pseudocode
+ lowercase words, convert string into array
+ map through them usinng charAt, the first letter of everyword capitalize and then slice and join"*/
+
+
+ //Another solution
+
+//  function cap(string){
+//     return string.toLowerCase().split(' ').map((char) => char[0].toUpperCase() + char.slice(1)).join(' ');
+//     //console.log(string)
+    
+//   }
+
+// solution # 3
+// function cap(string){
+//  words = [];
+
+//  for(let word in string.split(" ")){
+//      words.push(word[0].toUpperCase() + word.slice(1))
+//  }
+//     return words.join(' ');
+//   }
