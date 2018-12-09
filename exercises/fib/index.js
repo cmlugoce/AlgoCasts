f8d99a2ebcaa681fca9341f1ce6a5a740a2d9522
cmlugoce <cmlugoce@syr.edu>
2018-12-09 13:16:32 -0800

    add solution with recursion and memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//     let arr = [0, 1];
+//     for (let i = 2; i <= n; i++){
+//       arr.push(arr[i - 2] + arr[i -1])
+//     }
+//    return arr[n]
+// }
 
+
+
+//module.exports = fib;
+
+// using recursion & memoization
+
+function memoization(fn){
+   const cache = {};
+    return function (...args){
+      if (cache[args]) {
+          return cache[args];
+      }
+      const results = fn.apply(this, args);
+      cache[args] = results;
+
+      return results;
+    }; 
+
+}
+
+function fibSlow(n){
+    if (n < 2){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+const fib = memoization(fibSlow);
 module.exports = fib;
